985d43b55f86e372e2eb809b900aaf4a4b07fddb
Dayanand <120240023+Dayanand7031@users.noreply.github.com>
2025-01-20 02:13:13 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,5 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+export const url = 'https://food-delivery-back-8r1g.onrender.com';
+export const currency ="₹";
